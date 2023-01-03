4a305045a9f3845cab0a8dd01c0f92427858be76
Manish Pandey <manish.pandey.for.work@gmail.com>
2023-01-03 14:39:47 +0530

    GIT NEW PROCEDURE PARTIALLY ADDED

--- test1.sql
+++ test1.sql
@@ -13,3 +13,17 @@ JOIN Product ON Customers.Cus_id = Product.CUSTOMER_ID
 JOIN Product_Categories ON Product.prod_id = Product_Categories.P_PROD_ID;
 end;
 /
+create or replace PROCEDURE exchange_data 
+is
+emp_id employee_test.empid%type;
+emp_firstname employee_test.firstname%type;
+emp_lastname employee_test.lastname%type;
+emp_salary employee_test.salary%type;
+cursor getdetails is
+select empid,firstname,lastname,salary from employee_test;
+begin
+
+
+
+
+
