51bd5d8f2e3b0ea870394d9ed08afadaf7fd6dcc
Manish Pandey <manish.pandey.for.work@gmail.com>
2023-01-03 14:35:00 +0530

    PROCEDURE ADDED

--- test1.sql
+++ test1.sql
@@ -1,3 +1,15 @@
 select*from customers;
 commit;
 mmm;
+/
+create or replace procedure Data_Link
+is
+begin
+
+INSERT INTO CustomerProductCategory (CustomerName, ProductName, CategoryName)
+SELECT Customers.Customer_Name AS CustomerName, Product.Product_Name AS ProductName, Product_Categories.Category_Name AS CategoryName
+FROM Customers
+JOIN Product ON Customers.Cus_id = Product.CUSTOMER_ID
+JOIN Product_Categories ON Product.prod_id = Product_Categories.P_PROD_ID;
+end;
+/
